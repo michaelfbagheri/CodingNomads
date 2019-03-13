9106db2804dc9397f67cabfba873659f8eafbe94
mb480s <mike.f.bagheri@gmail.com>
2019-03-13 16:48:09 -0400

    lesson 2 complete

--- labs/02_basic_datatypes/02_06_invest.py
+++ labs/02_basic_datatypes/02_06_invest.py
@@ -7,3 +7,23 @@ Take in the following three values from the user:
 Print the future values to the console.
 
 '''
+
+from decimal import *
+total = input("investment amount: ")
+interest_rate = input("enter your interest rate %: ")
+years = input ("number of years you'd like to invest your funds: ")
+
+interest_rate = (float(interest_rate) / 100 ) + 1
+
+total = int(total) * 100
+
+i = 0
+
+while i < int(years):
+    total = int(total) * float(interest_rate)
+    i+= 1
+
+
+total = int(total)/100
+print("at the end of your term you'll have $" , total , ".")
+
